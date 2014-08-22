8d83674e57623e817b68f6cd0e8503c8e86e1be9
lee981115 <jungjunlee69@gmail.com>
2014-08-22 19:12:31 +0900

    Create SMARTHOME.c

---
+++ SMARTHOME.c
@@ -0,0 +1,19 @@
+#include <mega128.h>
+#include <ds1302.h>
+#include <lcd.h>
+#include <stdio.h>
+#define LD 630
+#define LE 683
+#define LED1_ON 'A'
+#define LED1_OFF 'a'
+#define LED2_ON 'B'
+#define LED2_OFF 'b'
+#define STOVE_ON 'C'
+#define STOVE_OFF 'c'
+int i,j,k;
+void servo_motter(short angle);
+void lcd_display(void);
+char wifi_rx(void);
+void rtc_init(void);
+void adc_senser(char no);
+void lamp_bright(char lux);
